f2da11211b3b5d5dae5e14fb39144a585467305a
aezBurkhal <44988851+aezBurkhal@users.noreply.github.com>
2018-12-12 00:14:12 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,33 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		//8 primitive types of variables in java
+		//variables hold values in a so to speak box.
+
+int  myNumber = 88;  //declaring and initializing variable at the same time 
+					// if you take out = 88, you will just be declaring variable
+short myShort = 847;
+long myLong = 9797;
+
+double myDouble = 7.4566;
+float myFloat = 324.3f;  //similar to double
+						//example of initializing variable in separate statement
+char myChar = 'y';
+boolean myBoolean = false;
+byte myByte = 127;
+myNumber = 88;
+
+System.out.println(myNumber);
+System.out.println(myShort);
+System.out.println(myLong);
+System.out.println(myDouble);
+System.out.println(myFloat);
+System.out.println(myChar);
+System.out.println(myBoolean);
+System.out.println(myByte);
+
+
+	}
+
+}
